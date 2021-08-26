f5a991a8d385df51c85ded4dea43ba907051d291
qixinbing <qixinbing@rongcloud.cn>
2021-08-26 14:36:49 +0800

    问题:增加 ios 代码 原因及方案: Reviewer: 来源:

---
+++ ios/RCUniIM/RCUniIM/RCUniIM.h
@@ -0,0 +1,15 @@
+//
+//  RCUniIM.h
+//  RCUniIM
+//
+//  Created by Qi on 2021/8/16.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "DCUniModule.h"
+
+@interface RCUniIM : DCUniModule
+
+@end
+
